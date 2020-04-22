54c49e4b70fd32ed38d4ac2c8969a50e2e81f6ac
Alan Couzens <alan@alancouzens.com>
2020-04-22 17:43:52 -0600

    commit

--- TIZ_linreg.py
+++ TIZ_linreg.py
@@ -1,8 +1,9 @@
-import numpy
-import sklearn
+import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 #Input variables time in zone (minutes) for Z1,Z2,Z3,Z4,Z5 per month
+#Note: This could also be done from a csv file (e.g. from Training Peaks) using pd.read_csv
+#Sample data
 time_in_zone_by_month=[
 [1205,902,330,48,20],
 [1303,1021,371,69,19],
@@ -11,7 +12,7 @@ time_in_zone_by_month=[
 [1333,1291,458,109,28],
 ]
 
-#Output variable - tested FTP each month
+#Output variable - tested FTP each month (sample data)
 FTP_by_month = [248, 279, 295, 310, 303]
 
 X = time_in_zone_by_month
@@ -20,7 +21,8 @@ y = FTP_by_month
 model = LinearRegression().fit(X,y)
 
 """Print out model weights for each zone so that you can see how strongly they are
-weighted/how much each minute in each zone is worth. You might be surprised!"""
+weighted/how much each minute in each zone is worth. e.g. the first coefficient is the
+weight for Zone 1, the second is the weight for Zone 2 etc."""
 print(model.coef_)
 print(model.intercept_)
 
